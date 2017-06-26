6c400ac6629b226a9d225e3f32f0cb88a4b398dd
moaible <motodera@moapp.info>
2017-06-26 17:47:55 +0900

    Rename package

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwAPIClient",
+    name: "SwiftyAPIRequest",
     dependencies: [
         .Package(url: "https://github.com/antitypical/Result.git", majorVersion: 3),
     ]
