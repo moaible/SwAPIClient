b14e48a38cbf22162f2d518781b8f0fbcb90eea1
moaible <motodera@moapp.info>
2017-06-27 08:55:13 +0900

    Add APIRequestAdapter protocol

---
+++ Sources/Adapters/APIRequestAdapter.swift
@@ -0,0 +1,14 @@
+//
+//  APIRequestAdapter.swift
+//  SwiftyAPIRequest
+//
+//  Created by Hiromi Motodera on 6/27/17.
+//  Copyright © 2017 moaible. All rights reserved.
+//
+
+import Foundation
+
+protocol APIRequestAdapter {
+    
+    
+}
